2dcee72c284beeaddd370fec846180a3e60ff6c3
izanami0511 <43243390+izanami0511@users.noreply.github.com>
2021-03-23 22:49:00 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask,render_template, url_for, request, redirect
+import sys
+import logging
 
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 @app.route('/', methods=['POST','GET'])
 def home():

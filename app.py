4ef75406a920c3b18831787f80f3e526066003ac
izanami0511 <43243390+izanami0511@users.noreply.github.com>
2021-03-23 22:39:05 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template, url_for, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST','GET'])
+def home():
+    global height
+    if request.method == 'POST':
+        height = request.form['height']
+        return redirect('/opr_ves')
+    else:
+        return render_template("index.html")
+
+@app.route('/opr_ves', methods=['GET','post'])
+def opr():
+    weight = int(height) - 110
+    height2 = (int(height) * 0.01) * (int(height) * 0.01)
+    index = weight/height2
+    return render_template("rost.html",weight = weight,index = index)
+if __name__ == "__main__":
+    app.run()
